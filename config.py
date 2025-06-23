313a6aef2226721d1c32709afa0e426fa2d4bb23
mkienne <mkien.dev@gmail.com>
2025-06-23 22:12:19 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,12 @@ class Config:
     """Cấu hình cơ bản cho ứng dụng"""
     
     # Database
-    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URL")
+    uri = os.getenv("DB_URL")
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = uri
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # CORS
